ec36cee7342656899daa3caa13ebba258aece9df
Ana Julia Neiverth <115558556+anajvn@users.noreply.github.com>
2024-02-27 12:53:10 -0300

    Build project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio-test'`, () => {
+  it(`should have as title 'Ana Julia Neiverth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-test');
+    expect(app.title).toEqual('Ana Julia Neiverth');
   });
 
   it('should render title', () => {
