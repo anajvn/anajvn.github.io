1b6ee2049aff20750048e7b6e2e76c3f4935fe54
Ana Julia Neiverth <115558556+anajvn@users.noreply.github.com>
2024-02-27 13:17:21 -0300

    Build project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Ana Julia Neiverth'`, () => {
+  it(`should have as title 'Ana Julia Neivert'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Ana Julia Neiverth');
+    expect(app.title).toEqual('Ana Julia Neivert');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio-test app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Portfolio app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { PageComponent } from './page/page.component';
 import { HeaderComponent } from './page/header/header.component';
 import { FooterComponent } from './page/footer/footer.component';
@@ -12,6 +10,7 @@ import { ProfileComponent } from './page/profile/profile.component';
 import { ExperienceComponent } from './page/experience/experience.component';
 import { ProjectComponent } from './page/project/project.component';
 import { ContactComponent } from './page/contact/contact.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,8 +26,7 @@ import { ContactComponent } from './page/contact/contact.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
